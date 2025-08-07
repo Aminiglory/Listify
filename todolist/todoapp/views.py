811207a1bdb4d66441ad63bbd44f7ab0bcc430ca
aminiglory <codetest670@gmail.com>
2025-08-07 20:45:20 +0200

    creation of the auth pages

--- todolist/todoapp/views.py
+++ todolist/todoapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_view(request):
+    return render(request,"auth/login.html")
+
+def register_view(request):
+    return render(request,"auth/register.html")
+
 def index(request):
     return render(request,"listify/index.html")
