a7880ddd361b48a19f2dd8064d864964f439e960
aminiglory <codetest670@gmail.com>
2025-08-07 22:42:51 +0200

    page setup and routing done

--- todolist/todoapp/urls.py
+++ todolist/todoapp/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path("",views.index,name='index'),
     path("login/",views.login_view,name='login'),
     path("register/",views.register,name='register'),
+    path("dashboard/",views.dashboard,name='dashboard'),
+    path("create-task/",views.createTask,name='createtask'),
+    path("display/",views.displayAllTasks,name="display"),
+    path("view-task/",views.viewTask,name="viewtask"),
+    path("edit-task/",views.editTask,name='edittask'),
+    path("delete-task/",views.deleteTask,name='deletetask'),
 ]
--- todolist/todoapp/views.py
+++ todolist/todoapp/views.py
@@ -10,3 +10,21 @@ def register(request):
 
 def index(request):
     return render(request,"listify/index.html")
+
+def dashboard(request):
+    return render(request,"listify/dashboard.html")
+
+def createTask(request):
+    return render(request,"listify/create.html")
+
+def displayAllTasks(request):
+    return render(request,"listify/display.html")
+
+def viewTask(request):
+    return render(request,"listify/view.html")
+
+def editTask(request):
+    return render(request,"listify/edit.html")
+
+def deleteTask(request):
+    return render(request,"listify/delete.html")
